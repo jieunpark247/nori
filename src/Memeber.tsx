cc4a8540003b6ea7bbf7c278c368a9ac524847ab
박지은F 광고플랫폼(YA) <parkjieun@yanolja.com>
2025-11-07 11:00:50 +0900

    test

--- src/Memeber.tsx
+++ src/Memeber.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { MyPageInfo } from './components/my/MyPageInfo';
 import { mockMypageData } from './components/my/mockData';
 
-export default function Mypage() {
+export default function Memeber() {
   return <MyPageInfo data={mockMypageData} />;
 }
