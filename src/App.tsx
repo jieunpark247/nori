9fbd0ebf76edd5a5f326ae8d97e30fe395954f7d
박지은F 광고플랫폼(YA) <parkjieun@yanolja.com>
2025-11-07 10:49:23 +0900

    수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import Header from './Header';
 import Nori from './Nori';
 import Home from './Home';
-import MyPage from './MyPage';
+import MyPage from './MyPages';
 
 export default function App() {
 	return (
--- src/MyPages.tsx
+++ src/MyPages.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { MyPage } from './components/mypage/MyPage';
 import { mockMypageData } from './components/mypage/mockData';
 
-export default function MyPagePage() {
+export default function MyPages() {
   return <MyPage data={mockMypageData} />;
 }
